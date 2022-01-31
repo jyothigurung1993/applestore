be7e6958932c90ccc0c6141680f619f0923130e8
Jyothi Gurung <jyothigurung@gmail.com>
2022-01-31 20:03:24 +0530

    Show the error messages

--- applestore/static/js/apple.js
+++ applestore/static/js/apple.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
     $(document).on('click', '#submit', function(e){
         e.preventDefault();
-        console.log("clicked")
         value = $("#product_id").val()
         console.log(value)
         $.ajax({
             type: 'GET',
             url: '/apple/get_product/'+value,
             success: function(response) {
-                console.log(response)
-            }
+                $("#api_res").text(response)
+            },
+            error: function(response) {
+                $("#api_res").text("Error, Please enter Valid ID")
+            },
         });
     })
 })
